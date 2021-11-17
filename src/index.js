71f6523b1d1ee953c1210f1da8465e2b25c9668c
mkhedre <mkhedre3@gmail.com>
2021-11-18 00:37:29 +0200

    edit port

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ const User = require('./models/user')
 require('./db/mongoose')
 
 const app = express()
-const port = process.env.port
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
 app.listen(port ,()=>{
-    console.log('conetcting to server',port)
+    console.log('conetcting to server',PORT)
 })
 // app.use((req,res,next)=>{
 //     if(req.method === 'GET'){
